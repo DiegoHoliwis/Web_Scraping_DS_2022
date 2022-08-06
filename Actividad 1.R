1a49651cc434184f5ac62b41e130218e2f9504dc
Camila Paredes <ceparedes@uc.cl>
2022-08-06 10:42:27 -0400

    .

--- Actividad 1.R
+++ Actividad 1.R
@@ -1,2 +1,9 @@
 # Workshop web scraping diplomado DS 2022 ' Secciones online
 # Diego Muñoz
+
+library(tidyverse)
+library(rvest)
+
+pagina <- read_html('https://www.mercadolibre.cl/ofertas?promotion_type=deal_of_the_day&container_id=MLC779365-1&page=1')
+
+
