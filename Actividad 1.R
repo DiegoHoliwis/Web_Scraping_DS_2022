8465c18029c0bda513608e90f10f254bc9b0a717
Camila Paredes <ceparedes@uc.cl>
2022-08-06 11:02:38 -0400

    .

--- Actividad 1.R
+++ Actividad 1.R
@@ -6,8 +6,8 @@ library(rvest)
 
 pagina <- read_html('https://www.mercadolibre.cl/ofertas?promotion_type=deal_of_the_day&container_id=MLC779365-1&page=1')
 
-
-pagina %>% 
+# Obtener el número de páginas de ofertas
+N_pagina <- pagina %>% 
   html_element(xpath = '//ul[@class = "andes-pagination"]') %>% 
   html_children() %>% 
   html_text2() %>% 
@@ -15,4 +15,33 @@ pagina %>%
   max(na.rm = TRUE) %>% 
   suppressWarnings()
 
-    
+
+# Obetner el titulo del producto
+# Forma 1: solo obtiene el primer producto....
+pagina %>% 
+  html_element(xpath = '//p[@class = "promotion-item__title"]') %>% 
+  html_text2()
+
+
+# Forma 2: Complicada, obtiene todo...
+
+1:48 %>% 
+  map_chr(.f = function(x){
+    pagina %>% 
+      html_element(xpath = paste0('//ol[@class = "items_container"]/li[',x,']//p[@class = "promotion-item__title"]')) %>% 
+      html_text2()
+  } )
+
+# Forma fácil
+pagina %>% 
+  html_elements(xpath = '//p[@class = "promotion-item__title"]') %>% 
+  html_text2()
+
+
+
+
+
+
+
+
+
