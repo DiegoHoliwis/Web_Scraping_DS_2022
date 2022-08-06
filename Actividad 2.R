50683d8112d4f8990a28072b6b0fd71be03dceb1
Camila Paredes <ceparedes@uc.cl>
2022-08-06 11:45:30 -0400

    .

--- Actividad 2.R
+++ Actividad 2.R
@@ -4,8 +4,38 @@ library(tidyverse)
 library(rvest)
 library(RSelenium)
 
+# Configuración inicial (siempre la misma)
 system("taskkill /im java.exe /f", intern=FALSE, ignore.stdout=FALSE) # Se finalizan los procesos Java
 
 driver <- RSelenium::rsDriver(browser = "chrome", 
                               chromever = "104.0.5112.79")
 remote_driver <- driver[["client"]]
+
+# Iniciamos el web scraping :D
+
+# Conectamos a la página web
+remote_driver$navigate("https://www.santander.cl/cotizador-web/")
+
+MARCA    = 'JEEP'
+MODELO   = 'RENEGADE'
+RUT      = '24312997-4'
+NOMBRE   = 'Diego Muñoz'
+FechaN   = '10/01/1997'
+SEXO     = 'Masculino'
+EMAIL    = 'correo_generico@gmail.com'
+TELEFONO = runif(1,900000000,999999999) %>% round(0) %>% as.character()
+
+# Pagina 1 -----
+# Seleccionar Marca
+
+remote_driver$findElement(using = 'xpath',
+                          value = '//select[@id = "marcas"]/option[@value = "78"]')$clickElement()
+
+
+
+
+
+
+
+
+
