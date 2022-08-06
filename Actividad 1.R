5abb1948f733bd73a3ade24db4e20fa4889865c7
Camila Paredes <ceparedes@uc.cl>
2022-08-06 11:24:57 -0400

    .

--- Actividad 1.R
+++ Actividad 1.R
@@ -73,5 +73,79 @@ Envio <- 1:length(Nombre) %>%
   })
 
 
+URL <- pagina %>% 
+  html_elements(xpath = '//a[@class = "promotion-item__link-container"]') %>% 
+  html_attr('href')
+
+
+# Function to scraping -----------------------
+pagina <- read_html('https://www.mercadolibre.cl/ofertas?promotion_type=deal_of_the_day&container_id=MLC779365-1&page=1')
+
+# Obtener el número de páginas de ofertas
+N_pagina <- pagina %>% 
+  html_element(xpath = '//ul[@class = "andes-pagination"]') %>% 
+  html_children() %>% 
+  html_text2() %>% 
+  as.numeric() %>% 
+  max(na.rm = TRUE) %>% 
+  suppressWarnings()
+
+df <- 1:N_pagina %>% 
+  map_dfr(.f = function(y){
+    pagina <- read_html(paste0('https://www.mercadolibre.cl/ofertas?promotion_type=deal_of_the_day&container_id=MLC779365-1&page=',y))
+    
+    Nombre <- pagina %>% 
+      html_elements(xpath = '//p[@class = "promotion-item__title"]') %>% 
+      html_text2()
+    
+    precio_old <- pagina %>% 
+      html_elements(xpath = '//span[@class = "promotion-item__oldprice"]') %>% 
+      html_text2() %>% 
+      str_remove_all('\\$ ') %>% 
+      str_remove_all('\\.') %>% 
+      as.numeric()
+    
+    precio_nuevo <- pagina %>% 
+      html_elements(xpath = '//span[@class = "promotion-item__price"]') %>% 
+      html_text2() %>% 
+      str_remove_all('\\$ ') %>% 
+      str_remove_all('\\.') %>% 
+      as.numeric()
+    
+    Envio <- 1:length(Nombre) %>% 
+      map_chr(.f = function(x){
+        pagina %>% 
+          html_element(xpath = paste0('//ol[@class = "items_container"]/li[',x,']//span[@class = "promotion-item__shipping"]')) %>% 
+          html_text2() %>% 
+          if_else(is.na(.),'Sin envio gratis',.)
+      })
+    
+    URL <- pagina %>% 
+      html_elements(xpath = '//a[@class = "promotion-item__link-container"]') %>% 
+      html_attr('href')
+    
+    Sys.sleep(runif(1,1,3))
+    
+    print(paste0('Pagina ',y,' completada'))
+    tibble(
+      Nombre,
+      precio_old,
+      precio_nuevo,
+      Envio,
+      URL)
+    
+  })
+
+df
+
+
+
+
+
+
+
+
+
+
 
 
